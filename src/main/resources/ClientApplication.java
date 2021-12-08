25340945ab08be473870338c9c6286619fd74016
sadegh-msm <ms.crysis1380@gmail.com>
2021-12-08 18:00:15 +0330

    adding client and server

--- src/main/resources/ClientApplication.java
+++ src/main/resources/ClientApplication.java
@@ -1,7 +1,71 @@
 package src.main.resources;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 /**
- * The type Client application.
+ * The Client application can connect a server on given port.
  */
 public class ClientApplication {
+    private Socket socket = null;
+    private DataInputStream input = null;
+    private DataOutputStream output = null;
+
+    /**
+     * Instantiates a new Client application for connecting to server.
+     *
+     * @param address the address
+     * @param port    the port
+     */
+    public ClientApplication(String address, int port) {
+        try {
+            socket = new Socket(address, port);
+            System.out.println("connected");
+
+            input = new DataInputStream(System.in);
+
+            output = new DataOutputStream(socket.getOutputStream());
+        } catch (UnknownHostException u) {
+            System.out.println(u);
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+
+        String line = "";
+
+        while (!line.equals("Over"))
+        {
+            try
+            {
+                line = input.readLine();
+                output.writeUTF(line);
+            }
+            catch(IOException i)
+            {
+                System.out.println(i);
+            }
+        }
+
+        try
+        {
+            input.close();
+            output.close();
+            socket.close();
+        }
+        catch(IOException i)
+        {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * Run and start a client on localhost and port 8000.
+     */
+    public void run() {
+        ClientApplication clientApplication = new ClientApplication("127.0.0.1", 8000);
+    }
 }
+
--- src/main/resources/ServerApplication.java
+++ src/main/resources/ServerApplication.java
@@ -1,7 +1,58 @@
 package src.main.resources;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
- * The type Server application.
+ * The Server application for clients to connect to it.
  */
 public class ServerApplication {
+    private Socket socket = null;
+    private ServerSocket server = null;
+    private DataInputStream input = null;
+
+    /**
+     * Instantiates a new Server application for clients to connect to it.
+     *
+     * @param port the port
+     */
+    public ServerApplication(int port) {
+        try {
+            server = new ServerSocket(port);
+            System.out.println("server is up!");
+
+            socket = server.accept();
+            System.out.println("client accepted");
+
+            input = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+
+            String line = "";
+
+            while (!line.equals("Over"))
+            {
+                try
+                {
+                    line = input.readUTF();
+                    System.out.println(line);
+
+                }
+                catch(IOException i)
+                {
+                    System.out.println(i);
+                }
+            }
+        } catch (IOException i) {
+            System.out.println(i);
+        }
+    }
+
+    /**
+     * Run and starts rhe server on port 8000.
+     */
+    public void run() {
+        ServerApplication serverApplication = new ServerApplication(8000);
+    }
 }
