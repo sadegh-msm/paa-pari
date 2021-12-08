6a42eb355044f1a3878fa40316aeb055d5e3794e
moeinahmadieh81 <77613346+moeinahmadieh81@users.noreply.github.com>
2021-12-08 12:20:01 +0330

    [feature] Client can tweet

---
+++ moein/Tweet.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Tweet {
+    private Account user;
+    private int likes;
+    private String content;
+    private LocalDate localDate;
+
+    public Tweet(){
+        user = new Account();
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+
+    public void setLikes(int likes) {
+        this.likes = likes;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        if(content.length() <= 256){
+            this.content = content;
+        }else {
+            System.out.println("Your content is more than 256 characters Enter again");
+            setContent(content);
+        }
+
+    }
+
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    public void setLocalDate() {
+        this.localDate = LocalDate.now();
+    }
+}
