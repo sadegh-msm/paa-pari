4538d6ff47f9769545876736837aeba73eaa2cce
moeinahmadieh81 <77613346+moeinahmadieh81@users.noreply.github.com>
2021-12-22 12:59:36 +0330

    feat : sign up method added

--- src/main/resources/Menu.java
+++ src/main/resources/Menu.java
@@ -4,32 +4,58 @@ package src.main.resources;
 import src.main.java.Account;
 import src.main.java.Pari;
 
+import java.util.Scanner;
+
+
 public class Menu {
     private Pari pari;
-    private Account user;
+    private Account account;
 
-    public void signIn(){
-
-    }
-    public void signUp(){
-
-    }
-    public void timeLine(){
-
+    public Menu(Pari pari, Account account) {
+        this.pari = pari;
+        this.account = account;
     }
 
-    public void Observe(){
+    public void signIn(String username, String password) {
+
+    }
+
+    public void signUp(String username, String password) {
+        System.out.println("Enter your First name");
+        Scanner scanner = new Scanner(System.in);
+        String fName = scanner.nextLine();
+        account.setFirstName(fName);
+        System.out.println("Enter your Last name");
+        String lName = scanner.nextLine();
+        account.setLastName(lName);
+        System.out.println("Write your biography");
+        System.out.println("Its length shouldn't be more than 256");
+        String bio;
+        bio = scanner.nextLine();
+        while (!account.setBiography(bio)) {
+            System.out.println("Enter again");
+            bio = scanner.nextLine();
+        }
+
+
+    }
+
+    public void timeLine() {
+
+    }
+
+    public void Observe() {
 
     }
 
     public void tweet() {
     }
 
-    public void reTweet(){
+    public void reTweet() {
 
     }
 
-    public void reply(){
+    public void reply() {
 
     }
 
