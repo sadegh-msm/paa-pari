ed866125dfff39f5d855c878d0261cd0b0b5dcce
sadegh-msm <ms.crysis1380@gmail.com>
2021-12-31 01:43:07 +0330

    changing main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 package src.main.java;
 
+import src.main.resources.ClientApplication;
 import src.main.resources.ServerApplication;
 
 import java.io.IOException;
@@ -7,10 +8,12 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         ServerApplication serverApplication = new ServerApplication();
+        ClientApplication clientApplication = new ClientApplication();
         try {
             serverApplication.startServer();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        clientApplication.StartClient();
     }
 }
